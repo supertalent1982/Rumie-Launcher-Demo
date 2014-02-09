04336bccf42c73995235337dfc76d0a38f94dcc7
Ed X201 <ed@4click.net>
2014-02-09 16:25:56 -0500

    videos running fullscreen now, using rumie logo for app

--- Ruime/app/build/source/r/release/com/jsdev/ruime/R.java
+++ Ruime/app/build/source/r/release/com/jsdev/ruime/R.java
@@ -64,13 +64,11 @@ public final class R {
     }
     public static final class raw {
         public static final int education=0x7f040000;
-        public static final int lexscreencast=0x7f040001;
-        public static final int play=0x7f040002;
-        public static final int screencast=0x7f040003;
-        public static final int video1=0x7f040004;
-        public static final int video2=0x7f040005;
-        public static final int video3=0x7f040006;
-        public static final int workedlowresscreencast=0x7f040007;
+        public static final int play=0x7f040001;
+        public static final int screencast=0x7f040002;
+        public static final int video1=0x7f040003;
+        public static final int video2=0x7f040004;
+        public static final int video3=0x7f040005;
     }
     public static final class string {
         public static final int action_settings=0x7f060000;
